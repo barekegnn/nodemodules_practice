5df4fb04d95e1a92aaa2746baeb04ab99756504f
Barekegn Asefa <barekegna@gmail.com>
2024-12-08 04:13:27 -0800

    using ES6 way to avoid global name space collision

---
+++ ES6-export.mjs
@@ -0,0 +1,7 @@
+function eat(){
+    console.log("human beings must eat in order to survive!")
+}
+function newton2ndLaw(){
+    console.log("newtons 2nd law states that, for every action there is an equal and opposite reaction");
+}
+export{eat, newton2ndLaw};
---
+++ ES6-import.mjs
@@ -0,0 +1,3 @@
+import { eat, newton2ndLaw } from "./ES6-export.mjs";
+eat();
+newton2ndLaw();
