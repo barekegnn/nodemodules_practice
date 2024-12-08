febf975016b1258ef0506bdf3d063649e93f4a8a
Barekegn Asefa <barekegna@gmail.com>
2024-12-08 01:39:46 -0800

    how to export a module using commonjs

---
+++ commonjs-export.js
@@ -0,0 +1,14 @@
+function sum(a, b) {
+  var c = a + b;
+  return c;
+}
+
+function divide(x, y) {
+  var g = x / y;
+  return g;
+}
+
+module.exports = {
+    sum,
+    divide,
+};
---
+++ commonjs-import.js
@@ -0,0 +1,4 @@
+var fromExport = require("./commonjs-export")
+const average = fromExport.sum(200, 250) / 2;
+console.log(average);
+
